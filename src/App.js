715b594b385e719d4536d3eba77fb828c30dfb58
DESKTOP-U2P2DON\KUCK_SU <kuck1106@naver.com>
2021-01-24 23:36:21 +0900

    plus

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Page1 from './page/page1';
+import Page2 from './page/page2';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+      <Router exact path="/" component={Page1}/>
+      </div>
   );
 }
 
